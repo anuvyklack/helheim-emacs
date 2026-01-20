211429e485f505e135dd572134534af4b812e3d5
Yuriy Artemyev <anuvyklack@gmail.com>
2026-01-20 20:52:35 +0300

    core: remake current line highlighting

--- user-lisp/helheim/keybindings/helheim-keybindings.el
+++ user-lisp/helheim/keybindings/helheim-keybindings.el
@@ -166,8 +166,6 @@
 
 ;;;; Magit-Section
 
-(add-hook 'magit-section-mode-hook 'helheim-disable-hl-line-mode)
-
 (with-eval-after-load 'magit-section
   (hel-keymap-set magit-section-mode-map
     "<tab>"     'magit-section-cycle
